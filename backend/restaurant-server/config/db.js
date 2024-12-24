020e9ff29e29667414278879fa51b6209c607720
maibui7879 <165109718+maibui7879@users.noreply.github.com>
2024-12-24 08:25:53 +0700

    Update db.js

--- backend/restaurant-server/config/db.js
+++ backend/restaurant-server/config/db.js
@@ -6,19 +6,5 @@ const db = mysql.createPool({
     password: '7879maibui', 
     database: 'restaurant'
 });
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err.message);
-        return;
-    }
-    console.log('Connected to MySQL database.');
-});
 module.exports = db;
